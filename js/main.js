625019a4fd0a9ab2a84d1894e1a0ffdb7eb279fb
Prakerja <therexxar@gmail.com>
2025-04-07 13:37:31 +0700

    update landing page home

--- js/main.js
+++ js/main.js
@@ -142,29 +142,33 @@ $(document).ready(function () {
 
 
 	//======== tiny slider
-    // tns({
-	// 	container: '.client-logo-carousel',
-	// 	autoplay: true,
-	// 	autoplayButtonOutput: false,
-	// 	mouseDrag: true,
-	// 	gutter: 15,
-	// 	nav: false,
-	// 	controls: false,
-	// 	responsive: {
-	// 		0: {
-	// 			items: 1,
-	// 		},
-	// 		540: {
-	// 			items: 2,
-	// 		},
-	// 		768: {
-	// 			items: 3,
-	// 		},
-	// 		992: {
-	// 			items: 4,
-	// 		}
-	// 	}
-	// });
+    tns({
+		container: '.client-logo-carousel',
+		autoplay: true,
+		autoplayButtonOutput: false,
+		mouseDrag: true,
+		gutter: 15,
+		nav: false,
+		controls: false,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			540: {
+				items: 2,
+			},
+			768: {
+				items: 3,
+			},
+			992: {
+				items: 4,
+			},
+			1170: {
+				items: 5,
+			}
+		}
+	});
+	
 
 
 	//WOW Scroll Spy
